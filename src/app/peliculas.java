ecacb337263cb89b5b8ed3d5712f5eaeabaad2e0
JESUS BARRIOS <jdbg20@gmail.com>
2020-07-30 15:50:29 -0500

    Version 0.1

---
+++ src/app/peliculas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/**
+ *
+ * @author JESUS
+ */
+public class peliculas {
+    
+    
+    
+    public static void main(String[] args){
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
